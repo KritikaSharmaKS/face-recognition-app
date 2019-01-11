2d6311a665cb091e9dc1cf4e2e908288a015be86
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-11 15:44:37 -0500

    style added to input component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,11 +4,11 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return (
-        <div className="">
-           <p>This app will detect faces in your images. Paste any link to an image below.</p>
-           <div>
-               <input type='text'></input>
-               <button>Detect</button>
+        <div>
+           <p className='f3'>This app will detect faces in your images. Paste any link to an image below.</p>
+           <div className='form center pa4 br3 shadow-5'>
+               <input className='f4 pa2 w-70 center' type='text'></input>
+               <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
            </div>
         </div>
     );
