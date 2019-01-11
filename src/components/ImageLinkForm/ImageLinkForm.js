2c06b0838a8105d818d9b11b20083cdd041d5ec7
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-11 16:08:25 -0500

    rank component added

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = () => {
     return (
         <div>
-           <p className='f3'>This app will detect faces in your images. Paste any link to an image below.</p>
+           <p className='f3 white b'>This app will detect faces in your images. Paste any link to an image below.</p>
            <div className='form center pa4 br3 shadow-5'>
                <input className='f4 pa2 w-70 center' type='text'></input>
                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div>
+                <p>KS, your rank is...</p>
+            </div>
+            <div>
+                <p>#1</p>
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
