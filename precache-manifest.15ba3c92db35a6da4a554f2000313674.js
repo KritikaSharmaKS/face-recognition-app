dd6f8c1a83242b09947825cbe7b1a9b11a86864b
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-11 12:57:12 -0500

    Updates

---
+++ precache-manifest.15ba3c92db35a6da4a554f2000313674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6de2bf8da835eb85162e",
+    "url": "/face-recognition-app/static/js/runtime~main.6de2bf8d.js"
+  },
+  {
+    "revision": "c68e955897836bb58909",
+    "url": "/face-recognition-app/static/js/main.c68e9558.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/face-recognition-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c68e955897836bb58909",
+    "url": "/face-recognition-app/static/css/main.3269e768.chunk.css"
+  },
+  {
+    "revision": "c50d571e2e26ab7d22adc11e451e01c2",
+    "url": "/face-recognition-app/index.html"
+  }
+];
