6d5bc4e09b9fefc631c51e7a39734fb8059a879f
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-12 00:12:05 -0500

    better styling

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,9 +6,11 @@ const ImageLinkForm = () => {
     return (
         <div>
            <p className='f3 white b'>This app will detect faces in your images. Paste any link to an image below.</p>
-           <div className='form center pa4 br3 shadow-5'>
-               <input className='f4 pa2 w-70 center' type='text'></input>
-               <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+           <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text'></input>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
            </div>
         </div>
     );
