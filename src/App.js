527176640379244b5e503bb3a08c0e1129b17b32
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-13 09:46:17 -0500

    input box and button event listeners added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,21 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor(){
+		super();
+		this.state = {
+			input: ''
+		}
+  }
+  
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  OnButtonSubmit = () => {
+    console.log("click");
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,7 +44,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} OnButtonSubmit={this.OnButtonSubmit}/>
         {/*<FaceRecognition /> */}
       </div>
     );
