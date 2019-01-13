18ed78a7ad008037b7b2f67f330897a5ba9a3396
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-13 18:52:22 -0500

    api connection working

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ class App extends Component {
   OnButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
     // Predict the contents of an image by passing in a URL.
-    app.models.predict(Clarifai.COLOR_MODEL, this.state.input)
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response => {
-        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       })
       .catch(err => {
         console.log(err);
