6aaf52dc2393d271cbd04029981f866abf6fc385
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-14 15:43:18 -0500

    image alt removed

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,9 +5,8 @@ const FaceRecognition = ({ imageUrl }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img src={imageUrl} alt="face" width='500px' height='auto'/>
+                <img src={imageUrl} alt="" width='500px' height='auto'/>
             </div>
-            
         </div>
     );
 }
