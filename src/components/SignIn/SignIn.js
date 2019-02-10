ac508b9602227a112706d117107d3e5dd047a094
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-02-10 14:19:15 -0500

    signin connected to backend

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,35 @@
 import React from 'react';
 
 class SignIn extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            signInEmail: '',
+            signInPassword: ''
+        }
+    }
+    onEmailChange = (event) => {
+        this.setState({signInEmail: event.target.value});
+    }
+    onPasswordChange = (event) => {
+        this.setState({signInPassword: event.target.value});
+    }
+    onSubmitSignIn = () => {
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })//post request
+        .then(response => response.json())
+        .then(data => {
+            if(data === 'success')
+                this.props.OnRouteChange('home');
+        });      
+    }
+
     render() {
         const  { OnRouteChange } = this.props;
         return (
@@ -11,18 +40,28 @@ class SignIn extends React.Component {
                             <legend className="f2 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f5" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                       type="email" 
+                                       name="email-address"  
+                                       id="email-address" 
+                                       onChange={this.onEmailChange}
+                                />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f5" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                       type="password" 
+                                       name="password"  
+                                       id="password" 
+                                       onChange={this.onPasswordChange}
+                                />
                             </div>
                         </fieldset>
                         <div className="">
                             <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib" 
                                    type="submit" 
                                    value="Sign in" 
-                                   onClick={()=>OnRouteChange('home')}
+                                   onClick={this.onSubmitSignIn}
                             />
                         </div>
                         <div className="lh-copy mt3">
